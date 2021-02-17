c81c3f430bd324bdf58f2ddc385f4c25aa38c2eb
Laura Chan <chan@bryter.io>
2021-02-17 22:47:45 +0000

    feat: add link to a highlighted text

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,43 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "notionify" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "notionify" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'notionify.addLink',
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const clipboardText = await vscode.env.clipboard.readText();
+        const selection = editor.selection;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('notionify.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+        if (selection.isEmpty || !clipboardText.startsWith('http')) {
+          editor.edit((editBuilder) => {
+            editBuilder.replace(selection, clipboardText);
+          });
+          return;
+        }
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from notionify!');
-	});
+        if (clipboardText.startsWith('http')) {
+          const document = editor.document;
 
-	context.subscriptions.push(disposable);
+          const selectedText = document.getText(selection);
+          const selectedTextWithHyperlink = `[${selectedText}](${clipboardText})`;
+
+          editor.edit((editBuilder) => {
+            editBuilder.replace(selection, selectedTextWithHyperlink);
+          });
+        }
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
