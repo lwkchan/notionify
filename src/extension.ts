d4d1c8e5e93b984725ec30cca5f0cc92b36f38ff
Laura Chan <chan@bryter.io>
2021-02-18 17:59:42 +0000

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,9 @@
 import * as vscode from 'vscode';
+import { replaceSelection } from './utils';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "notionify" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     'notionify.addLink',
     async () => {
@@ -17,21 +13,15 @@ export function activate(context: vscode.ExtensionContext) {
         const selection = editor.selection;
 
         if (selection.isEmpty || !clipboardText.startsWith('http')) {
-          editor.edit((editBuilder) => {
-            editBuilder.replace(selection, clipboardText);
-          });
+          replaceSelection(editor, selection, clipboardText);
           return;
         }
 
         if (clipboardText.startsWith('http')) {
           const document = editor.document;
-
           const selectedText = document.getText(selection);
           const selectedTextWithHyperlink = `[${selectedText}](${clipboardText})`;
-
-          editor.edit((editBuilder) => {
-            editBuilder.replace(selection, selectedTextWithHyperlink);
-          });
+          replaceSelection(editor, selection, selectedTextWithHyperlink);
         }
       }
     }
---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+import * as vscode from 'vscode';
+
+export const replaceSelection = (
+  editor: vscode.TextEditor,
+  selection: vscode.Selection,
+  newText: string
+) => {
+  editor.edit((editBuilder) => {
+    editBuilder.replace(selection, newText);
+  });
+};
